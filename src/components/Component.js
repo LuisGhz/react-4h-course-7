ca16271f564b2a1fb528fe580aa36e5e4c538dd4
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-08 18:31:00 -0500

    Create count with useState and fakeFetch fakeFetch increments count by one every 2 seconds

--- src/components/Component.js
+++ src/components/Component.js
@@ -1,8 +1,17 @@
-
+import { useState } from 'react';
 const Component = () => {
+  const [count, setCount] = useState(0);
+
+  const fakeFetch = () => {
+    setTimeout(() => {
+      setCount(count + 1);
+    }, 2000);
+  }
+
   return (
     <div>
-
+      <h1>Component counter: { count }</h1>
+      <button onClick={ fakeFetch }>fake fetch</button>
     </div>
   )
 }
